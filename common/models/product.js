d958b98061917372b9850482285cf4e2355af48c
venkatvr123 <venkatvr897@gmail.com>
2018-08-10 21:50:56 +0530

    Add a remoteMethod to filter between price ranges

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,49 @@
 'use strict';
 
 module.exports = function(Product) {
+  Product.getByPrice = function(req, res) {
+    if (req.body) {
+      Product.find({
+        fields: {
+          productName: true,
+        },
+        where: {
+          and: [
+            {sellingPrice: {
+              between: [
+                req.body.startPrice,
+                req.body.endPrice,
+              ],
+            },
+            },
+          ],
+        },
+      }, function(err, instance) {
+        if (instance) {
+          let att = [];
+          instance.map((item) => {
+            att.push(item.__data.productName);
+          });
+          if (att.length) {
+            res.json(att);
+          } else {
+            res.json([]);
+          }
+        } else {
+          res.json();
+        }
+      });
+    } else {
+      res.json([]);
+    }
+  };
 
+  Product.remoteMethod('getByPrice', {
+    accepts: [
+        {arg: 'req', type: 'object', http: {source: 'req'}},
+        {arg: 'res', type: 'object', http: {source: 'res'}},
+    ],
+    returns: {root: true, type: 'object'},
+    http: {path: '/getByPrice', verb: 'post'},
+  });
 };
